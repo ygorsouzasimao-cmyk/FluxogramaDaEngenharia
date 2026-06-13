7e39f3d3ef7377ed075d03eda9c45d98c1245681
ygor <ygorsouzasimao@gmail.com>
2026-06-12 22:07:17 -0300

    engenharia

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+﻿Console.WriteLine("--- Fluxograma Da Engenharia ---\n");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
